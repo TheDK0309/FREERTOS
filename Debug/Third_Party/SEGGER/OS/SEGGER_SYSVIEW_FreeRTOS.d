47cf7dbc2f6ce2239a9d9a2b202bc45055581ef2
Do Le Tuan Minh <32629360+TheDK0309@users.noreply.github.com>
2019-08-06 18:20:26 +0300

    Add files via upload

---
+++ Debug/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.d
@@ -0,0 +1,41 @@
+Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.o: \
+ ../Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.c \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Config/FreeRTOSConfig.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/task.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/list.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/Config/Global.h \
+ ../Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Config/FreeRTOSConfig.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/portable.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/task.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/list.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/Config/Global.h:
+
+../Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
