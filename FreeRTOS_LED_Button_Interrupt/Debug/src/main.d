24ce6cea7fc7fc481514c195ed458d62dc3e0bc0
Do Le Tuan Minh <32629360+TheDK0309@users.noreply.github.com>
2019-08-06 17:57:41 +0300

    Add files via upload

---
+++ FreeRTOS_LED_Button_Interrupt/Debug/src/main.d
@@ -0,0 +1,145 @@
+src/main.o: ../src/main.c \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/device/stm32f4xx.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cm4.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cmInstr.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cmFunc.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cmSimd.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/misc.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dma2d.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_ltdc.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_sai.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_qspi.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_fmpi2c.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_spdifrx.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_cec.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Config/FreeRTOSConfig.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/task.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/list.h
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/device/stm32f4xx.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cm4.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cmInstr.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cmFunc.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core/core_cmSimd.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/misc.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_dma2d.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_ltdc.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_sai.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_qspi.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_fmpi2c.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_spdifrx.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc/stm32f4xx_cec.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Config/FreeRTOSConfig.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/portable.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/task.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/list.h:
---
+++ FreeRTOS_LED_Button_Interrupt/Debug/src/subdir.mk
@@ -0,0 +1,28 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c \
+../src/system_stm32f4xx.c 
+
+OBJS += \
+./src/main.o \
+./src/system_stm32f4xx.o 
+
+C_DEPS += \
+./src/main.d \
+./src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DUSE_SEMIHOSTING -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -DDEBUG -DSTM32F446xx -DUSE_STDPERIPH_DRIVER -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/SEGGER/Config" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/SEGGER/OS" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/SEGGER/SEGGER" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Config" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/inc" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/device" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
