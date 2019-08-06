535e49ba999c2e82ec9af3ba390f217d5de85a92
Do Le Tuan Minh <32629360+TheDK0309@users.noreply.github.com>
2019-08-06 17:55:40 +0300

    Add files via upload

---
+++ FreeRTOS_LED_Button_Interrupt/Debug/Third_Party/FreeRTOS/org/Source/list.d
@@ -0,0 +1,26 @@
+Third_Party/FreeRTOS/org/Source/list.o: \
+ ../Third_Party/FreeRTOS/org/Source/list.c \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Config/FreeRTOSConfig.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/list.h
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Config/FreeRTOSConfig.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/portable.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include/list.h:
---
+++ FreeRTOS_LED_Button_Interrupt/Debug/Third_Party/SEGGER/Config/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.c 
+
+OBJS += \
+./Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.o 
+
+C_DEPS += \
+./Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Third_Party/SEGGER/Config/%.o: ../Third_Party/SEGGER/Config/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DUSE_SEMIHOSTING -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -DDEBUG -DSTM32F446xx -DUSE_STDPERIPH_DRIVER -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/StdPeriph_Driver/inc" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/SEGGER/Config" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/SEGGER/OS" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/SEGGER/SEGGER" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Third_Party/FreeRTOS/org/Source/include" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/Config" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/inc" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/device" -I"C:/Users/Minhd/workspace/FreeRTOS_LED_Button_Interrupt/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
