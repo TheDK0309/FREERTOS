fea1296a0333c15866b3d40aa8726ce556557c36
Do Le Tuan Minh <32629360+TheDK0309@users.noreply.github.com>
2019-08-06 18:01:00 +0300

    Add files via upload

---
+++ FreeRTOS_Queue_Processing/Debug/Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.d
@@ -0,0 +1,32 @@
+Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.o: \
+ ../Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.c \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Config/FreeRTOSConfig.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/Config/Global.h
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Config/FreeRTOSConfig.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/portable.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/SEGGER/SEGGER.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/Config/Global.h:
---
+++ FreeRTOS_Queue_Processing/Debug/Third_Party/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,8 @@
+Third_Party/SEGGER/SEGGER/SEGGER_RTT.o: \
+ ../Third_Party/SEGGER/SEGGER/SEGGER_RTT.c \
+ ../Third_Party/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/Config/SEGGER_RTT_Conf.h
+
+../Third_Party/SEGGER/SEGGER/SEGGER_RTT.h:
+
+C:/Users/Minhd/workspace/FreeRTOS_Test/Third_Party/SEGGER/Config/SEGGER_RTT_Conf.h:
